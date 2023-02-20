499d2b6bdf47403f4bcaf533946badbfc7fa1bc4
Bartosz Budka <95640@office365.ukw.edu.pl>
2023-02-20 10:48:12 +0100

    Dodanie klasy Filmy i Kategoria

---
+++ Models/Film.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Filmy.Models
+{
+    public class Film
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Dodaj tytul")]
+        [StringLength(50)]
+        public string Tytul { get; set; }
+        [Required(ErrorMessage = "Dodaj Rezysera")]
+        [StringLength(50)]
+        public string Rezyser { get; set; }
+        [Required(ErrorMessage = "Dodaj Opis")]
+        [StringLength(200)]
+        public string Opis { get; set; }
+        public decimal? Cena { get; set; }
+        public DateTime DataDodania { get; set; }
+        public int KategoriaID { get; set; }
+        public Kategoria Kategoria { get; set; }
+    }
+}
