f056457e5599804a8ae4e72869f3930354976bc8
Bartosz Budka <95640@office365.ukw.edu.pl>
2023-02-20 14:42:52 +0100

    aktualizacjaZ domu dziala

---
+++ DAL/FilmyContext.cs
@@ -0,0 +1,20 @@
+﻿using Filmy.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Filmy.DAL
+{
+    public class FilmyContext : DbContext
+    {
+        public DbSet<Film>Filmy { get; set; }
+        public DbSet <Kategoria> kategorie { get; set; }
+
+        public FilmyContext(DbContextOptions<FilmyContext> options): base(options)
+        {
+
+        }
+    }
+}
